db77b18f8a82b05a879954594d3e5e09fa32a8b3
babywhale03 <inthemode@naver.com>
2023-05-19 18:18:47 +0900

    [05.19] #10.10

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
+import { graphql } from "gatsby";
+import { link } from "fs";
 
-function Blog() {
-    return (
-        <Layout title="Blog">
-          <p>The most recent news from my shop.</p>
-        </Layout>
-    );
+export default function Blog({data}) {
+  console.log(data);
+  return (
+    <Layout title="Blog">
+      <ul>
+        {data.allFile.nodes.map((file, index) => (
+          <li>{file.name}</li>
+        ))}
+      </ul>
+    </Layout>
+  );
 }
 
+export const query = graphql`
+  query BlogTitles {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
+
 export const Head = () => <Seo title="Blog" />;
-export default Blog;
