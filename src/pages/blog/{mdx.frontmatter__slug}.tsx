f469b8909540af044be5b23fc942d371cb054124
babywhale03 <inthemode@naver.com>
2023-05-20 02:42:27 +0900

    [05.19] #10.15

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import Layout from "../../components/Layout";
 import { graphql } from "gatsby";
 import Seo from "../../components/Seo";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 interface IBlogPostProps {
-    data: Queries.PostDetailQuery;
-    children: any;
+  data: Queries.PostDetailQuery;
+  children: any;
 }
 
 export default function BlogPost({ data, children }: IBlogPostProps) {
+  const image = getImage(data.mdx?.frontmatter?.headerImage?.gatsbyImageData!);
   return (
     <Layout title="">
+      <GatsbyImage image={image as any} alt={data.mdx?.frontmatter?.title} />
       <div>{children}</div>
     </Layout>
   );
@@ -26,11 +29,16 @@ export const query = graphql`
         date
         title
         slug
+        headerImage {
+          childImageSharp {
+            gatsbyImageData(height: 450, placeholder: BLURRED)
+          }
+        }
       }
     }
   }
 `;
 
 export const Head = ({ data }: IBlogPostProps) => (
-    <Seo title={data.mdx?.frontmatter?.title} />
+  <Seo title={data.mdx?.frontmatter?.title} />
 );
